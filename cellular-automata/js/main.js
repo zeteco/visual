43d0b2463a1b1e6d0a5159f6dc521a434d6af541
Stefan Huber <sh@signalwerk.ch>
2016-09-15 23:39:14 +0200

    MOD: bring the code to a js file

--- cellular-automata/js/main.js
+++ cellular-automata/js/main.js
@@ -1,3 +1,4 @@
+<<<<<<< 649b1af6898ff2a2eb049d5983449445ffbc2ab8
 // var ruleInput = document.querySelector('input.rule')
 // var containers = document.body.querySelectorAll('.container')
 var containers = Array.prototype.slice.call(document.querySelectorAll('.container'));
@@ -12,6 +13,26 @@ var rndBool = () => Math.random() < .5
 
 // make array of random numbers
 var rndRow = count => Array.from(Array(count)).map(rndBool);
+=======
+var ruleInput = document.querySelector('#rule')
+var container = document.body.querySelector('.container')
+
+var width = 120
+var height = 120
+var ruleNumber
+
+// make random number 0 or 1
+var rndBool = () => Math.random()<.5
+
+// make array of random numbers
+var rndRow = count => {
+  let randomRow = []
+  for (var i = 0; i < count; i++) {
+    randomRow.push(rndBool())
+  }
+  return randomRow
+}
+>>>>>>> MOD: bring the code to a js file
 
 // make array of numbers for results
 var makeRule = number => {
@@ -19,10 +40,35 @@ var makeRule = number => {
   while (binary.length < 8) {
     binary = "0" + binary
   }
+<<<<<<< 649b1af6898ff2a2eb049d5983449445ffbc2ab8
   binary = binary.split('').reverse()
   return binary
 }
 
+=======
+  binary = binary.split("").reverse()
+  return binary
+}
+
+// makes new divs and puts cells in row
+var renderRow = row => {
+  var rowDiv = newRowDiv()
+  row.map( bool => {
+    var cell = document.createElement('div')
+    cell.classList.add('cell')
+    cell.classList.add( bool ? 'cell--true' : 'cell--false' )
+    rowDiv.appendChild(cell)
+  })
+}
+// makes new row.div
+var newRowDiv = () => {
+  var rowDiv = document.createElement('div')
+  rowDiv.classList.add('row')
+  container.appendChild(rowDiv)
+  return rowDiv
+}
+
+>>>>>>> MOD: bring the code to a js file
 // meat
 var calcNewRow = (oldRow, ruleNumber) => {
   var newRow = []
@@ -33,16 +79,24 @@ var calcNewRow = (oldRow, ruleNumber) => {
     var right = + arr[index + 1]
     if (index === 0)
       var left = + arr[arr.length - 1]
+<<<<<<< 649b1af6898ff2a2eb049d5983449445ffbc2ab8
     if (index === arr.length - 1)
       var right = + arr[0]
 
     var res = parseInt(('' + left + self + right), 2)
+=======
+    if (index === arr.length)
+      var right = + arr[0]
+
+    var res = parseInt(("" + left + self + right), 2)
+>>>>>>> MOD: bring the code to a js file
     newRow.push( + rule[res])
   })
   return newRow
 }
 
 // exec
+<<<<<<< 649b1af6898ff2a2eb049d5983449445ffbc2ab8
 var calculate = ruleNumber => {
   var result = []
   // ruleNumber = ruleInput.value || 73
@@ -130,3 +184,17 @@ var newCA = elements => {
 
 
 newCA(containers)
+=======
+var calculate = () => {
+  container.innerHTML = ''
+  ruleNumber = ruleInput.value || 73
+  var lastRow = rndRow(width)
+  for (var i = 0; i < height; i++) {
+    var nextRow = calcNewRow(lastRow, ruleNumber)
+    renderRow(nextRow)
+    lastRow = nextRow
+  }
+}
+
+calculate()
+>>>>>>> MOD: bring the code to a js file
